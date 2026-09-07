671854a0219a46b77d180eb47faa33268c4cd5ab
abdulmueedbooley <abdulmueed.booley@gmail.com>
2026-09-07 15:49:44 +0200

    commit

--- blog.py
+++ blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import Registration_Form, Login_Form
 app = Flask(__name__) #special variable in python that is the name of the module
+
+app.config['SECRET_KEY'] = '3192e70280e26652984eaf747a84d6d2'
 
 # List of dictionaries that represents a single blog post
 posts = [
@@ -27,6 +30,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = Registration_Form()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # name of home page function
+    return render_template('register.html', title='register', form=form)
+
+@app.route("/login")
+def login_form():
+    form = Login_Form()
+    return render_template('login.html', title='Login', form=form)
 
 # So flask knows where to look for static and template files
 # This condition is only true if the script is ran directly
